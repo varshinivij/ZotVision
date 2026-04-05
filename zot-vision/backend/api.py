42a78dbca2e1ae9951eeab470837c638fbce434a
Varshini Vijay <varshinivijay05@gmail.com>
2026-04-04 17:59:37 -0700

    post

--- zot-vision/backend/api.py
+++ zot-vision/backend/api.py
@@ -6,8 +6,15 @@ app = Flask(__name__)
 def create_fire_fighter_manager(model_path="", num_firefighters=4):
     return FireFighterManager(model_path, num_firefighters)
 
-@app.route('/handle_post', methods=['POST'])
+@app.route('/handle_post1', methods=['POST'])
 def handle_post():
+    data = request.get_json()
+    print("Received POST data:", data)
+    return "POST request received"
+
+
+@app.route('/handle_post2', methods=['POST'])
+def handle_post2():
     data = request.get_json()
     print("Received POST data:", data)
     return "POST request received"
